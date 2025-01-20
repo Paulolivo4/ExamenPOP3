e918c49ab474af61b16bb0802131487ffc31cc91
cisco <cisco@UPEAULA-31704>
2025-01-20 09:46:58 -0500

    Agregar archivos de proyecto.

---
+++ Models/Cocktail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPOP3.Models
+{
+    public class Cocktail
+    {
+        public string IdDrink { get; set; }
+        public string StrDrink { get; set; }
+        public string StrCategory { get; set; }
+        public string StrInstructions { get; set; }
+        public string StrDrinkThumb { get; set; }
+
+    }
+}
---
+++ Services/CocktailService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using ExamenPOP3.Models;
+
+namespace ExamenPOP3.Services
+{
+    public class CocktailService
+    {
+        private readonly HttpClient _httpClient;
+        public CocktailService()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<List<Cocktail>> GetCocktails(string query)
+        {
+            var url = $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={query}";
+            var response = await _httpClient.GetFromJsonAsync<Dictionary<string, List<Cocktail>>>(url);
+            return response?["drinks"] ?? new List<Cocktail>();
+        }
+
+    }
+}
